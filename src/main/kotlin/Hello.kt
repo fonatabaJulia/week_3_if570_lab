6d7a1c7ad3658a58fd04796b28af8eace6e4d714
fonataba_julia <julia.theresia@student.umn.ac.id>
2023-02-23 22:57:50 +0700

    Exploring default values and compact functions

--- src/main/kotlin/Hello.kt
+++ src/main/kotlin/Hello.kt
@@ -40,8 +40,31 @@ fun feedTheFish(){
     val day = randomDay()
     val food = fishFood(day)
     println("Today is $day and the fish eat $food")
+    println("Change water: ${shouldChangeWater(day)}")
 
 }
 fun main(args: Array<String>) {
     feedTheFish()
+    swim()
+    swim("slow")
+    swim(speed="turtle-like")
 }
+
+fun swim(speed: String = "fast") {
+    println("swimming $speed")
+}
+
+fun shouldChangeWater (day: String, temperature: Int = 22, dirty: Int = 22 ): Boolean {
+    return when {
+        isTooHot(temperature) -> true
+        isDirty(dirty) ->true
+        isSunday(day) -> true
+        else -> false
+    }
+}
+
+fun isTooHot(temperature: Int) = temperature > 30
+
+fun isDirty(dirty: Int) = dirty > 30
+
+fun isSunday(day: String) = day == "Sunday"
