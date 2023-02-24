b8edb353242afcec4fd5641c5b639e9323fb3a57
fonataba_julia <julia.theresia@student.umn.ac.id>
2023-02-24 13:37:48 +0700

    Adding class constructor

--- src/main/kotlin/example/myapp/Aquarium.kt
+++ src/main/kotlin/example/myapp/Aquarium.kt
@@ -1,15 +1,51 @@
 package example.myapp
 
-class Aquarium {
-        var width: Int = 20
-        var height: Int = 40
-        var length: Int = 100
+//class Aquarium(length: Int = 100, width: Int = 20, height: Int = 40) {
+//
+//        var length: Int = length
+//        var width: Int = width
+//        var height: Int = height
+//
+//        fun printSize() {
+//
+//
+//                println(
+//                        "length: $length cm" +
+//                                " width: $width cm " +
+//                                " height: $height cm"
+//                )
+//        }
 
-        fun printSize(){
+//}
 
-                println("Width: $width cm" +
-                        "Length: $length cm " +
-                        "Heigth: $height cm")
+
+
+class Aquarium (var length: Int = 100, var width: Int = 20, var height: Int =
+40) {
+    var volume: Int
+        get() = width * height * length / 1000
+        set(value) {
+            height = (value * 1000) / (width * length)
         }
+    init {
+        println("aquarium initializing")
+    }
+//    init {
+//        // 1 liter = 1000 cm^3
+//        println("Volume: ${width * length * height / 1000} liters")
+//    }
+    fun printSize() {
+        println("Width: $width cm " +
+                "Length: $length cm " +
+                "Height: $height cm "
+        )
+
+        println("Volume: $volume liters")
+    }
+    constructor(numberOfFish: Int) : this() {
+        val tank = numberOfFish * 2000 * 1.1
+        height = (tank / (length * width)).toInt()
+    }
+
 }
 
